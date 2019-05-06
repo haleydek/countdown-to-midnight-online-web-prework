905c23e9d5ec254912c6e2688903d1ef068c8938
Haley DeKeyser <hcd09@live.com>
2019-05-06 02:30:17 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,16 @@
 #write your code here
 
-def countdown
+def countdown(number)
+  counter = 11
+  while counter > 1
+    counter -= 1
+    puts "#{counter} SECOND(S)!"
+  end
+  return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(number)
+  5.times do
+    sleep(1)
+  end
 end
