f0a6a074a65ae84d7410aa7939e55500f4d87cc2
Haley DeKeyser <hcd09@live.com>
2019-05-06 02:12:37 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,10 @@
 #write your code here
 
-def countdown
+def countdown(number)
+  counter = 10
+  while seconds > 0
+    counter -= 1
+    puts "#{number} SECOND(S)!"
+  end
+  puts "HAPPY NEW YEAR!"
 end
